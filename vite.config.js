c6717ff97a0042a85f911bc23f48743626fdbfb7
jlc96707 <jlc96707@uga.edu>
2026-09-14 16:34:38 -0400

    fixing config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/ieee-fycintern-tracker/',
+  base: '/ieee-internal-platform/',
 })
